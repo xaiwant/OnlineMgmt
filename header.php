22b405b6cd52c77dc58c5544ad4beb716482a0a7
xaiwant <jaywanttopno@yahoo.com>
2018-04-09 14:08:11 +0530

    Bootstrap Available locally.

--- header.php
+++ header.php
@@ -34,16 +34,14 @@ include('Session.php');
         <title>
             <?php echo $title; ?>
         </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-        <link rel="shortcut icon" href="images/favicon.ico" />
-        <script src="js/Register.js"></script>
-    </head>
-
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+           <script src="js/jquery.min.js"></script>
+           <script src="js/bootstrap.min.js"></script>
+	       <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+           <link rel="stylesheet" type="text/css" href="css/styles.css">
+	       <script src="js/Register.js"></script>
+   </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
@@ -74,4 +72,4 @@ include('Session.php');
             </div>
         </nav>
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center justify-content-center">
