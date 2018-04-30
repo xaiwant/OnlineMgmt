5f782c1840ccb8717c3a535e49bbd22f7a27fdc1
xaiwant <jaywanttopno@yahoo.com>
2018-04-30 16:10:28 +0530

    User Registration passing strip_tags

--- UserRegistration.php
+++ UserRegistration.php
@@ -128,24 +128,21 @@ if (empty($_SESSION)) {
 
 <?php
 
-
-
-
 if (isset($_POST["mob_number"]) && isset($_POST["email"])) {
     $sql = "SELECT * from Users where mob_number='" . $_POST["mob_number"] . "' AND email='" . $_POST["email"] . "'";
     if (mysqli_num_rows(mysqli_query($conn, $sql)) == 0) {
         
 		/*Generating unique PatientID*/
 		if ($_POST["user_role"] == 'patient') {
-          $patient_id = 'PAT' . substr($_POST["mob_number"], -4) . '' . substr($_POST["firstname"], 0, 2) . '' . substr($_POST["lastname"], 0, 2) . '' . substr($_POST["DatOfBirth"], 0, 2) . '' . date('His', time());
+          $patient_id = 'PAT' . substr(strip_tags($_POST["mob_number"], -4)) . '' . substr(strip_tags($_POST["firstname"], 0, 2)) . '' . substr(strip_tags($_POST["lastname"], 0, 2)) . '' . substr(strip_tags($_POST["DatOfBirth"], 0, 2)) . '' . date('His', time());
 		} else {
-		  $patient_id = 'DOC' . substr($_POST["mob_number"], -4) . '' . substr($_POST["firstname"], 0, 2) . '' . substr($_POST["lastname"], 0, 2) . '' . substr($_POST["DatOfBirth"], 0, 2) . '' . date('His', time());	
+		  $patient_id = 'DOC' . substr(strip_tags($_POST["mob_number"], -4)) . '' . substr(strip_tags($_POST["firstname"], 0, 2)) . '' . substr(strip_tags($_POST["lastname"], 0, 2)) . '' . substr(strip_tags($_POST["DatOfBirth"], 0, 2)) . '' . date('His', time());	
 		}
 		
         $query      = "INSERT INTO Users (patientid, firstname, middlename, lastname, DatOfBirth, gender, mob_number, email, pass1, pass2, user_role, blood_group, height, weight, createdtime,secuirtyq, secuirtya )
-						VALUES ('" . strtoupper($patient_id) . "', '" . $_POST["firstname"] . "','" . $_POST["middlename"] . "','" . $_POST["lastname"] . "','" . $_POST["DatOfBirth"] . "','" . $_POST["gender"] . "',
-						'" . $_POST["mob_number"] . "','" . $_POST["email"] . "','" . md5($_POST["pass1"]) . "','" . md5($_POST["pass2"]) . "','" . $_POST["user_role"] . "','" . $_POST["blood_group"] . "','" . $_POST["height"] . "',
-						'" . $_POST["weight"] . "', '" . time() . "', '" . $_POST["secuirtyq"] . "', '" . $_POST["secuirtya"] . "')";
+						VALUES ('" . strtoupper($patient_id) . "', '" . strip_tags($_POST["firstname"]) . "','" . strip_tags($_POST["middlename"]) . "','" . strip_tags($_POST["lastname"]) . "','" . $_POST["DatOfBirth"] . "','" . $_POST["gender"] . "',
+						'" . strip_tags($_POST["mob_number"]) . "','" . strip_tags($_POST["email"]) . "','" . strip_tags(md5($_POST["pass1"])) . "','" . strip_tags(md5($_POST["pass2"])) . "','" . $_POST["user_role"] . "','" . $_POST["blood_group"] . "','" . strip_tags($_POST["height"]) . "',
+						'" . strip_tags($_POST["weight"]) . "', '" . time() . "', '" . $_POST["secuirtyq"] . "', '" . strip_tags($_POST["secuirtya"]) . "')";
         
         mysqli_query($conn, $query);
 ?>		
